9ec9edd0d6df974b59b48c1eb8f680d682bd3b32
sjiinkim <116076204+sjiinkim@users.noreply.github.com>
2026-02-03 06:04:18 +0000

    Deploying to gh-pages from @ sjiinkim/sjiinkim.github.io@ffa489bd473d2144138133c3aba4250b69d61d3b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "sjiinkim/seojinkim.github.io",
+    "data-repo": "sjiinkim/sjiinkim.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
